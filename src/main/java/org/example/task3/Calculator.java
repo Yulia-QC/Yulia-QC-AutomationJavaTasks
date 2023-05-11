b0765f8f30dae8c4c08d9eacb2eb2b73bb424b46
Yuliia <yteren@softserveinc.com>
2023-05-11 12:19:57 +0200

    Calculator task3 completed

--- src/main/java/org/example/task3/Calculator.java
+++ src/main/java/org/example/task3/Calculator.java
@@ -1,29 +1,33 @@
 package org.example.task3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
     public static final String NUMBER_REQUIRED = "Input should be a number";
-
+    public static Scanner reader;
+    public static char operator;
 
     public static void main(String[] args) {
         double num1;
         double num2;
         double ans;
-        char op;
+
 
 
         try {
-            Scanner reader = new Scanner(System.in);
+            reader = new Scanner(System.in);
             System.out.print("Input first number: ");
+            verifyDouble();
             num1 = reader.nextDouble();
             System.out.print("Input an operator (e.g. +, -, *, /, %: ");
-            op = reader.next().charAt(0);
+            verifyOperator();
             System.out.print("Input second number: ");
+            verifyDouble();
             num2 = reader.nextDouble();
 
 
-            switch (op) {
+            switch (operator) {
                 case '+':
                     ans = num1 + num2;
                     break;
@@ -44,16 +48,39 @@ public class Calculator {
                     return;
             }
 
-            System.out.print("Input second number: ");
-
-
 
             System.out.println("The result is given as follows: ");
-            System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
-        } catch (Exception e) {
-            System.out.println(NUMBER_REQUIRED);
-
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + ans);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed");
         }
 
+
     }
+    private static void verifyDouble() {
+        while (!reader.hasNextDouble()) {
+            System.out.println(NUMBER_REQUIRED);
+            reader.nextLine();
+            System.out.print("Input a number: ");
+        }
+    }
+
+    private static void verifyOperator() {
+        boolean isSpecifiedOperator = false;
+        while (!isSpecifiedOperator) {
+            try {
+                operator = reader.next().charAt(0);
+                if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%') {
+                    System.out.print("Operator is incorrect. Please specify from list: ");
+                    continue;
+                }
+                isSpecifiedOperator = true;
+                reader.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.print("Operator is incorrect");
+
+            }
+        }
+    }
+
 }
